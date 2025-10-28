3180997645e2714c6d6458fd187e138bf479a38b
Kumarsaro57 <148423093+Kumarsaro57@users.noreply.github.com>
2025-10-28 05:04:35 -0700

    variables

---
+++ variables.py
@@ -0,0 +1,94 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+======= RESTART: C:/Users/ELCOT/Desktop/SARAVANA/New folder/variables.py =======
+Traceback (most recent call last):
+  File "C:/Users/ELCOT/Desktop/SARAVANA/New folder/variables.py", line 6, in <module>
+    isstudent=false
+NameError: name 'false' is not defined. Did you mean: 'False'?
+
+======= RESTART: C:/Users/ELCOT/Desktop/SARAVANA/New folder/variables.py =======
+name Saravana kumar
+<class 'type'> Saravana kumar
+age 22
+<class 'type'> 22
+height 6.2
+<class 'type'> 6.2
+weight 45
+<class 'type'> 45
+Traceback (most recent call last):
+  File "C:/Users/ELCOT/Desktop/SARAVANA/New folder/variables.py", line 18, in <module>
+    print("is studend",student)
+NameError: name 'student' is not defined. Did you mean: 'isstudent'?
+
+======= RESTART: C:/Users/ELCOT/Desktop/SARAVANA/New folder/variables.py =======
+name Saravana kumar
+<class 'str'>
+age 22
+<class 'int'>
+height 6.2
+<class 'float'>
+weight 45
+<class 'int'>
+Traceback (most recent call last):
+  File "C:/Users/ELCOT/Desktop/SARAVANA/New folder/variables.py", line 18, in <module>
+    print("is studend",student)
+NameError: name 'student' is not defined. Did you mean: 'isstudent'?
+
+======= RESTART: C:/Users/ELCOT/Desktop/SARAVANA/New folder/variables.py =======
+name Saravana kumar
+<class 'str'>
+age 22
+<class 'int'>
+height 6.2
+<class 'float'>
+weight 45
+<class 'int'>
+isstudend false
+<class 'str'>
+marks 453
+<class 'int'>
+pi 3.14
+<class 'str'>
+shoolname st.britto.hr.sec.school
+<class 'str'>
+>>> 
+================================================= RESTART: C:/Users/ELCOT/Desktop/SARAVANA/New folder/variables.py ================================================
+name Saravana kumar
+<class 'str'>
+age 22
+<class 'int'>
+height 6.2
+<class 'float'>
+weight 45
+<class 'int'>
+isstudend false
+<class 'str'>
+marks 453
+<class 'int'>
+pi 3.14
+<class 'str'>
+shoolname st.britto.hr.sec.school
+<class 'str'>
+mytext Hello
+<class 'type'> Hello
+>>> 
+================================================= RESTART: C:/Users/ELCOT/Desktop/SARAVANA/New folder/variables.py ================================================
+name Saravana kumar
+<class 'str'>
+age 22
+<class 'int'>
+height 6.2
+<class 'float'>
+weight 45
+<class 'int'>
+isstudend false
+<class 'str'>
+marks 453
+<class 'int'>
+pi 3.14
+<class 'str'>
+shoolname st.britto.hr.sec.school
+<class 'str'>
+mytext Hello
+<class 'str'>
