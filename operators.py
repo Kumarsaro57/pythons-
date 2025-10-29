a7f2e04741dc1ba5a99cfc9cfa8f02f3c273c2b7
Kumarsaro57 <148423093+Kumarsaro57@users.noreply.github.com>
2025-10-29 08:54:30 -0700

    operators

---
+++ operators.py
@@ -0,0 +1,27 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+======== RESTART: C:/Users/ELCOT/Documents/project/operator/operator.py ========
+enter the number12
+enter the number7
+enter the number1.5
+19
+5
+84
+1.7142857142857142
+1
+144
+343
+6.833333333333333
+a= 7 b= 1.5 c= 7
+<class 'builtin_function_or_method'>
+13.5
+False
+False
+20.5 6.833333333333333
+90.0
+1.26
+75
+positive
+total_cost 18.0
+19 5 84 1.7142857142857142
