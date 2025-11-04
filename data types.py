dd57c916eb96bfd6f4d4877f7f29ac292dc8ec67
Kumarsaro57 <148423093+Kumarsaro57@users.noreply.github.com>
2025-11-04 03:21:40 -0800

    condition statements

---
+++ data types.py
@@ -0,0 +1,25 @@
+name ="saravana"
+height =175.5
+singlechild= True
+address=("k.pudur madurai-7",)
+hobbies=["dance,games"]
+
+print(type(name),type(height),type(singlechild),type(address),type(hobbies))
+print()
+
+
+name ="surya"
+age =22
+print(f"my name is ",(name),"and my age",(age))
+print()
+
+a =20
+b= 10
+print("sum:",a+b)
+print("diff:",a-b)
+print("prod:",a*b)
+print("quot:",a/b)
+print()
+
+
+
